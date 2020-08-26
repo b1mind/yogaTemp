d989fff92d3a61cd2caa2409d23c34e1a49fddaf
b1mind <brent@1mindsolutions.com>
2020-08-26 01:39:06 -0600

    📱mobile nav complete ⛔need more nav styles+hover

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 import './app.js'
+
+const nav = document.querySelector('.navMain')
+const canHazBurger = document.querySelector('.canHazBurger')
+
+canHazBurger.addEventListener('click', e => {
+	nav.classList.toggle('hidden')
+})
